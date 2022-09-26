9bc991a678ed0387bce80d6eb34b64302a34af7f
Ilya Korolev <korolev.iv@hotmail.com>
2022-09-26 17:54:19 +0300

    HW13 is done

--- HW12/HW12/Program.cs
+++ HW12/HW12/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace HW12;
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace HW12;
 
 static class Program
 {
@@ -9,27 +12,42 @@ static class Program
 
     public class Shop
     {
-        private List<Item> _items = new List<Item>();
+        public readonly ObservableCollection<Item> Items = new ObservableCollection<Item>();
 
         public void Add(Item item)
         {
-            _items.Add(item);
+            Items.Add(item);
         }
 
         public void Remove(Item item)
         {
-            _items.Remove(item);
+            Items.Remove(item);
         }
+
     }
 
     public class Customer
     {
-
+        
+        void OnItemChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add: // если добавление
+                    if (e.NewItems?[0] is Item newItem)
+                        Console.WriteLine($"Добавлен новый товар: {newItem.Name}");
+                    break;
+                case NotifyCollectionChangedAction.Remove: // если удаление
+                    if (e.OldItems?[0] is Item oldItem)
+                        Console.WriteLine($"Удален товар: {oldItem.Name}");
+                    break;
+            }
+        }
     }
 
     public class Item
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
--- HW13/HW13/Program.cs
+++ HW13/HW13/Program.cs
@@ -4,15 +4,33 @@ static class Program
 {
     static void Main(string[] args)
     {
-        var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        // Инициализируем первую коллекцию
+        var intList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-        var newList = list.Top(30);
-        Console.WriteLine(string.Join(" ", newList));
+        // Инициализируем вторую коллекцию
+        var objList = new List<Person> { 
+            new Person(1, 15),
+            new Person(2, 45),
+            new Person(3, 35),
+            new Person(4, 59),
+            new Person(5, 10),
+            new Person(6, 17),
+            new Person(7, 55),
+            new Person(8, 67),
+            new Person(9, 3)  
+        };
 
-        var newList1 = list.Top(30, person => person.Age);
-        Console.WriteLine(string.Join(" ", newList1));
+        // Вызываем мепрую перегрузку
+        var newIntList = intList.Top(30);
+        Console.WriteLine(string.Join(" ", newIntList));
+
+        // Вызываем вторую перегрузку
+        var newObjList = objList.Top(30, person => person.Age);
+        Console.WriteLine(string.Join(" ", newObjList));
 
     }
+    
+    // Первая перешрузка
     public static IEnumerable<int> Top(this IEnumerable<int> items, double X)
     {
         if (X > 100 || X < 0)
@@ -26,15 +44,40 @@ static class Program
         return items.Reverse().Take((int)countToReturn);
     }
 
-    public static IEnumerable<int> Top(this IEnumerable<int> items, double X, PersonExtractor person)
+    // Вторая перегрузка
+    public static IEnumerable<Person> Top(this IEnumerable<Person> items, double X, AgeExtractor func)
     {
-        return items;
+        var sortedList = items.OrderByDescending(s => func(s));
+
+        if (X > 100 || X < 0)
+        {
+            throw new ArgumentException();
+        }
+
+        var count = X / 100 * items.Count();
+        var countToReturn = Math.Ceiling(count);
+
+        return sortedList.Take((int)countToReturn);
     }
 
-    public delegate string PersonExtractor(int person);
+    // Описываем делегат
+    public delegate int AgeExtractor(Person person);
 
-    class Person
+    // Описываем класс Person
+    public class Person
     {
+        public int Id { get; set; }
         public int Age { get; set; }
+        public Person(int id, int age)
+        {
+            Id = id;
+            Age = age;
+        }
+
+        // Переопределяем метод ToString() для корректного вывода списока объектов
+        public override string ToString()
+        {
+            return "Id" + Id + "-Age" + Age + ";";
+        }
     }
 }
